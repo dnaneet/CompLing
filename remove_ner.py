7283005755d5a6bba53ced03e8da92f1fb802693
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2023-05-01 22:23:35 -0400

    Update remove_ner.py

--- remove_ner.py
+++ remove_ner.py
@@ -43,6 +43,29 @@ st.text_area('', txt_clean)
 #st.write(txt_clean)
 #st.write("\n")
 #st.write("## Named entities")
+
+st.write("The following entity libraries may be selected")
+PERSON - People, including fictional.
+
+st.write("""
+NORP - Nationalities or religious or political groups. 
+FAC - Buildings, airports, highways, bridges, etc.  
+ORG - Companies, agencies, institutions, etc.  
+GPE - Countries, cities, states.  
+LOC - Non-GPE locations, mountain ranges, bodies of water.  
+PRODUCT - Objects, vehicles, foods, etc. (Not services.)  
+EVENT - Named hurricanes, battles, wars, sports events, etc. 
+WORK_OF_ART - Titles of books, songs, etc.  
+LAW - Named documents made into laws.  
+LANGUAGE - Any named language.  
+DATE - Absolute or relative dates or periods.  
+TIME - Times smaller than a day.  
+PERCENT - Percentage, including "%".  
+MONEY - Monetary values, including unit.  
+QUANTITY - Measurements, as of weight or distance.  
+ORDINAL - "first", "second", etc.  
+CARDINAL - Numerals that do not fall under another type.""")
+         
 doc = nlp(txt)
 visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
 
