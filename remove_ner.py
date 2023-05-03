2bda0c3ce318d9e63ee6e2347b3f107603981ded
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2023-05-03 11:25:44 -0400

    Update remove_ner.py

--- remove_ner.py
+++ remove_ner.py
@@ -13,37 +13,9 @@ from spacy_streamlit import visualize_ner
 
 #USER DEFINED FUNCTIONS
 
-# Create a list of words to be replaced
-def replace_words(paragraph, words):
-  """Replaces a list of words in a paragraph with blank spaces.
-
-  Args:
-    paragraph: The paragraph to be modified.
-    words: A list of words to be replaced.
-
-  Returns:
-    The modified paragraph.
-  """
-
-  # Split the paragraph into words.
-  words_in_paragraph = paragraph.split()
-
-  # Loop over the words in the paragraph.
-  for index, word in enumerate(words_in_paragraph):
-    # If the word is in the list of words to be replaced, replace it with a blank space.
-    if word in words:
-      words_in_paragraph[index] = " "
-   
-  words_in_paragraph = [item.replace("Team", "team") for item in words_in_paragraph]   
-  # Join the words back into a paragraph.
-  new_paragraph = " ".join(words_in_paragraph)
-
-  # Return the new paragraph.
-  return new_paragraph
-  
 
 def replace_ner(mytxt):
-    clean_text = mytxt    
+    clean_text = mytxt
     doc = nlp(mytxt)
     for ent in reversed(doc.ents):
         clean_text = clean_text[:ent.start_char] +ent.label_ + clean_text[ent.end_char:]
@@ -64,9 +36,7 @@ st.sidebar.markdown("## The de-identification process uses [standard Python libr
 
 st.write("### Enter the text you wish to deidentify in the text box below and click 'ctrl + enter.'")
 txt = st.text_area('', """  """)
-txt2 = replace_words(txt, ["Senior", "Capstone", "Design", "senior","capstone","design"])
-#st.write(txt2)
-txt_clean = replace_ner(txt2).replace('ORG','ACME Co.').replace('CARDINAL','999')
+txt_clean = replace_ner(txt).replace('ORG','ACME Co.')
 st.write("### Deidentified text")
 st.write("You may copy and paste this de-identified text once it appears.")
 st.write(txt_clean)
@@ -76,7 +46,7 @@ st.write(txt_clean)
 #st.write("## Named entities")
 
         
-doc = nlp(txt2)
+doc = nlp(txt)
 visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
 
 st.write("The following entity libraries may be selected")
